b3babc2e12a50389af362465c7af1e0fade41b04
Swapnil <kadamswapnil91@gmail.com>
2020-06-17 15:36:15 +0530

    hide navigation bar

--- task 1.1/ViewController.swift
+++ task 1.1/ViewController.swift
@@ -114,16 +114,16 @@ class ViewController: UIViewController ,UITextFieldDelegate{
     
     
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.isNavigationBarHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
 
 
-}
+
+
+
 
